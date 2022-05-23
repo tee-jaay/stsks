73b056277dc2134ec3ba7ee22eeb7d95b64e8a11
TeeJaay <tamjid.mdh@gmail.com>
2022-05-24 00:36:07 +0600

    directories

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// colors
+const stPrimaryColor = Color(0xFF0C9869);
+const stTextColor = Color(0xFF3C4046);
+const stBackgroundColor = Color(0xFFF9F8FD);
+// spacing
+const double stDefaultSpace = 16.0;
