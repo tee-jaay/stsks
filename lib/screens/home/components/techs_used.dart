89f325525ce0bf4aaa2658044a720a9590ca0390
Tam Jid <tamjid.mdh@gmail.com>
2022-06-12 16:30:20 +0600

    tech neames list view

--- lib/screens/home/components/techs_used.dart
+++ lib/screens/home/components/techs_used.dart
@@ -1,50 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:badges/badges.dart';
+
 import 'package:stasks/constants.dart';
 
 import 'package:stasks/screens/home/components/section_title.dart';
 
 class TechsUsed extends StatelessWidget {
-  const TechsUsed({Key? key}) : super(key: key);
+  final List<String> _techNames = [
+    'flutter',
+    'express.js',
+    'react.js',
+    'mongoDb',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: stDefaultSpace),
-        child: Column(
-          children: [
-            Container(
-              child: SectionTitle(
-                title: "Techs used",
+    return Material(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: stDefaultSpace,
+            vertical: stDefaultSpace * 2,
+          ),
+          child: Column(
+            children: [
+              Container(
+                child: SectionTitle(
+                  title: "Techs used",
+                ),
               ),
-            ),
-            SizedBox(
-              height: stDefaultSpace * 4,
-            ),
-            Row(
-              children: [
-                customTechChip(
-                  text: "express.js",
+              SizedBox(
+                height: stDefaultSpace * 4,
+              ),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(
+                    _techNames.length,
+                    (index) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Chip(
+                        padding: EdgeInsets.all(0),
+                        backgroundColor: Color(0xff2E2E2E),
+                        label: Text(
+                          _techNames[index],
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  // [
+
+                  // Spacer(),
+                  // customTechChip(
+                  //   text: "flutter",
+                  // ),
+                  // ],
                 ),
-                Spacer(),
-                customTechChip(
-                  text: "react.js",
-                ),
-                Spacer(),
-                customTechChip(
-                  text: "mongoDB",
-                ),
-                Spacer(),
-                customTechChip(
-                  text: "flutter",
-                ),
-              ],
-            ),
-            SizedBox(
-              height: stDefaultSpace * 4,
-            ),
-          ],
+              ),
+              SizedBox(
+                height: stDefaultSpace * 4,
+              ),
+            ],
+          ),
         ),
       ),
     );
