19e537b917538dc526d6047a11e26465b3d38f45
Tam Jid <tamjid.mdh@gmail.com>
2022-06-16 15:23:29 +0600

    dashboard Body()

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:stasks/constants.dart';
-import 'package:date_format/date_format.dart';
+import 'package:stasks/screens/dashboard/body.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({Key? key}) : super(key: key);
@@ -12,26 +11,13 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
-  DateTime todayDate = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Dashboard"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(stDefaultSpace),
-        child: Row(
-          children: [
-            const Text(
-              "Welcome to Dashboard",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            const Spacer(),
-            Text(formatDate(todayDate, [yy, '-', M, '-', d])),
-          ],
-        ),
-      ),
+      body: Body(),
     );
   }
 }
