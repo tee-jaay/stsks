5b39573e89ca4bdd84061cb670dbfa4e66aef83e
Tam Jid <tamjid.mdh@gmail.com>
2022-08-01 23:16:19 +0600

    projects list view

--- lib/screens/dashboard/projects/projects_list/inc/body.dart
+++ lib/screens/dashboard/projects/projects_list/inc/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stasks/constants.dart';
+import 'package:stasks/screens/dashboard/projects/projects_list/inc/project_card.dart';
 
 class Body extends StatefulWidget {
   const Body({Key? key}) : super(key: key);
@@ -11,30 +12,11 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(stDefaultSpace),
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: 200.0,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.fill,
-                image: NetworkImage("https://picsum.photos/200/300"),
-              ),
-            ),
-            child: Text(
-              "Project title",
-              style: TextStyle(
-                color: Colors.white,
-                backgroundColor: Colors.black.withOpacity(0.5),
-                fontSize: 18.0,
-              ),
-            ),
-          ),
-        ),
-      ],
+    return ListView.builder(
+        itemCount: 8,
+        itemBuilder: (context, index) => ProjectCard(
+              key: Key(index.toString()),
+            )
     );
   }
 }
