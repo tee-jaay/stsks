9366ef036a1f10f2d1adfcf3f6ce92ca6a8f6f04
Tam Jid <tamjid.mdh@gmail.com>
2023-01-21 01:24:14 +0600

    auth ctl loading

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,1 +1,19 @@
-class AuthController {}
+import 'package:flutter/foundation.dart';
+
+class AuthController with ChangeNotifier{
+  bool _loading = false;
+
+  bool get loading => _loading;
+
+  set loading(bool value) {
+    _loading = value;
+  }
+
+  Future<void> signUp(Object obj) async {
+    print(obj);
+  }
+
+  Future<void> signIn(Object obj) async {
+    print(obj);
+  }
+}
