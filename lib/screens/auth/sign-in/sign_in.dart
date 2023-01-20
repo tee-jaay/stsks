17fae3799e54541d45291c68c6357de5d09aea20
Tam Jid <tamjid.mdh@gmail.com>
2023-01-21 00:54:10 +0600

    home screen val states

--- lib/screens/auth/sign-in/sign_in.dart
+++ lib/screens/auth/sign-in/sign_in.dart
@@ -1,18 +1,132 @@
 import 'package:flutter/material.dart';
 
-class SignInScreen extends StatelessWidget {
+import '../sign-up/sign_up.dart';
+import '../../../settings/constants.dart';
+
+class SignInScreen extends StatefulWidget {
   const SignInScreen({Key? key}) : super(key: key);
   static String screenId = "sign_in";
 
   @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  late String _email = '';
+  late String _password = '';
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  FocusNode _emailFocusNode = FocusNode();
+  FocusNode _passwordFocusNode = FocusNode();
+
+  void _handleSubmit() {
+    final isValid = _formKey.currentState?.validate();
+    if (!isValid!) {
+      return;
+    }
+    print('sign in');
+    print(_email);
+    print(_password);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text(
-          "Sign In",
-          style: TextStyle(
-            color: Colors.black87,
-            fontSize: 22.0,
+      body: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: _emailController,
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
+                      return 'Email is required';
+                    } else if (!regExpEmail.hasMatch(value)) {
+                      return 'Email is invalid';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) => setState(() {
+                    _email = value;
+                  }),
+                  decoration: const InputDecoration(hintText: 'Email'),
+                  keyboardType: TextInputType.emailAddress,
+                  focusNode: _emailFocusNode,
+                  onFieldSubmitted: (value) =>
+                      FocusScope.of(context).requestFocus(_passwordFocusNode),
+                ),
+                SizedBox(
+                  height: appDefaultSpace,
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
+                      return 'Password is required';
+                    } else if (!regExpPassword.hasMatch(value)) {
+                      return 'Password is weak';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) => setState(() {
+                    _password = value;
+                  }),
+                  decoration: const InputDecoration(hintText: 'Password'),
+                  obscureText: true,
+                  focusNode: _passwordFocusNode,
+                ),
+                SizedBox(
+                  height: appDefaultSpace,
+                ),
+                TextButton(
+                  onPressed: _handleSubmit,
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll(Colors.blueAccent),
+                  ),
+                ),
+                SizedBox(
+                  height: appDefaultSpace,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, SignUpScreen.screenId);
+                  },
+                  child: RichText(
+                    text: TextSpan(
+                        text: 'Don\'t have an account?',
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontStyle: FontStyle.italic,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: ' Sign Up',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blueAccent,
+                            ),
+                          ),
+                          const TextSpan(text: ' here'),
+                        ]),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/auth/sign-up/sign_up.dart
+++ lib/screens/auth/sign-up/sign_up.dart
@@ -1,18 +1,106 @@
 import 'package:flutter/material.dart';
 
-class SignUpScreen extends StatelessWidget {
+import '../../../settings/constants.dart';
+
+class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
   static String screenId = "sign_up";
 
   @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  late String _email = '';
+  late String _password = '';
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  FocusNode _emailFocusNode = FocusNode();
+  FocusNode _passwordFocusNode = FocusNode();
+
+  void _handleSubmit() {
+    final isValid = _formKey.currentState?.validate();
+    if (!isValid!) {
+      return;
+    }
+    print('sign up');
+    print(_email);
+    print(_password);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Sign Up",
-          style: TextStyle(
-            color: Colors.black87,
-            fontSize: 22.0,
+    return Scaffold(
+      body: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: _emailController,
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
+                      return 'Email is required';
+                    } else if (!regExpEmail.hasMatch(value)) {
+                      return 'Email is invalid';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) => setState(() {
+                    _email = value;
+                  }),
+                  decoration: InputDecoration(hintText: 'Email'),
+                  keyboardType: TextInputType.emailAddress,
+                  focusNode: _emailFocusNode,
+                  onFieldSubmitted: (value) =>
+                      FocusScope.of(context).requestFocus(_passwordFocusNode),
+                ),
+                SizedBox(
+                  height: appDefaultSpace,
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
+                      return 'Password is required';
+                    } else if (!regExpPassword.hasMatch(value)) {
+                      return 'Password is weak';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) => setState(() {
+                    _password = value;
+                  }),
+                  decoration: InputDecoration(hintText: 'Password'),
+                  obscureText: true,
+                  focusNode: _passwordFocusNode,
+                ),
+                SizedBox(
+                  height: appDefaultSpace,
+                ),
+                TextButton(
+                  onPressed: _handleSubmit,
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll(Colors.blueAccent),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
