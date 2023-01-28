5bd57c1c3f381a84774d083955eb960359e33769
Tam Jid <tamjid.mdh@gmail.com>
2023-01-28 19:49:38 +0600

    task status, priority, ontap

--- lib/screens/dashboard/projects/task/list/inc/body.dart
+++ lib/screens/dashboard/projects/task/list/inc/body.dart
@@ -21,32 +21,57 @@ class Body extends StatelessWidget {
       child: SizedBox(
         height: MediaQuery.of(context).size.height * 0.8,
         child: FutureBuilder(
-          builder: (context, snapshot) =>
-              snapshot.connectionState == ConnectionState.waiting
-                  ? const Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : RefreshIndicator(
-                      onRefresh: () =>
-                          _refreshProjectTasks(context, projectId, accessToken),
-                      child: Consumer<TaskController>(
-                        builder: (context, value, child) => ListView.builder(
-                          itemCount: value.tasksPreviews.length,
-                          itemBuilder: (context, index) {
-                            return Card(
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 16.0, horizontal: 8.0),
-                                child: Text(
-                                  value.tasksPreviews[index].title,
-                                  style: TextStyle(color: Colors.black87),
-                                ),
-                              ),
-                            );
+          builder: (context, snapshot) => snapshot.connectionState ==
+                  ConnectionState.waiting
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : RefreshIndicator(
+                  onRefresh: () =>
+                      _refreshProjectTasks(context, projectId, accessToken),
+                  child: Consumer<TaskController>(
+                    builder: (context, value, child) => ListView.builder(
+                      itemCount: value.tasksPreviews.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          onTap: (){
+                            print(value.tasksPreviews[index].id);
+                            print(value.tasksPreviews[index].projectId);
                           },
-                        ),
-                      ),
+                          child: Card(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 16.0, horizontal: 8.0),
+                              child: Row(
+                                children: [
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        value.tasksPreviews[index].title,
+                                        style: TextStyle(color: Colors.black87),
+                                      ),
+                                      Text(
+                                        value.tasksPreviews[index].status,
+                                        style: TextStyle(color: Colors.cyan),
+                                      ),
+                                    ],
+                                  ),
+                                  Spacer(),
+                                  Text(
+                                    'Priority: ${value.tasksPreviews[index].priority}',
+                                    style: TextStyle(
+                                        color: Colors.black87,),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
                     ),
+                  ),
+                ),
           future: _refreshProjectTasks(context, projectId, accessToken),
         ),
       ),
